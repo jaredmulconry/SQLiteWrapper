8af816ce230333821d161b4013bb4ee6b65360aa
Jared Mulconry <j.mulconry@atomizergames.com>
2015-05-11 00:26:18 +1000

    Added a comment block at the top of each file with the licence and a summary of the purpose of the wrapper.

--- src/SQLiteWrapped.cpp
+++ src/SQLiteWrapped.cpp
@@ -1,3 +1,35 @@
+/*
+Licence:
+	The MIT License (MIT)
+
+	Copyright (c) 2015 Jared Mulconry
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+Purpose:
+	A very thin wrapper around SQLite3, written in C++.
+	The C interface of SQLite3 is a good interface for the most part.
+	The goal of this wrapper is to add type safety where it is useful,
+	apply RAII to outputs and generally make the library cleaner to call 
+	from C++.  
+*/
+
 #include "SQLiteWrapped.hpp"
 #include <stdexcept>
 #include <type_traits>
